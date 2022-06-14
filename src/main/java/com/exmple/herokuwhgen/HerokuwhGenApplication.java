7df7e5941f960705d4990f3bebb8ee796a9e522d
Hello Jonas <hello.jonas@2ibi.com>
2022-06-14 14:07:30 +0200

    init

---
+++ src/main/java/com/exmple/herokuwhgen/HerokuwhGenApplication.java
@@ -0,0 +1,13 @@
+package com.exmple.herokuwhgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuwhGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuwhGenApplication.class, args);
+    }
+
+}
